55ef3a16de2d4c07bac9f792bdef1c37db3ce60b
Anderson Zango <azango94@gmail.com>
2023-12-22 19:47:13 +0200

    Update Exception Handling

--- src/main/java/com/ReservaVoos/ExceptionHandler/ControllerExceptionHandler.java
+++ src/main/java/com/ReservaVoos/ExceptionHandler/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ReservaVoos.ExceptionHandler;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import com.ReservaVoos.DTORequest.ExceptionDTO;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,12 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity existingDocument(DataIntegrityViolationException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO("User already exists!");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatAllExceptions(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("An Error as ocurred, please revise the data");
         return ResponseEntity.badRequest().body(exceptionDTO);
     }
 
